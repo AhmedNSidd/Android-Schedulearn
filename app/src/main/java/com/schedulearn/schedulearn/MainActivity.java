0a3cc0fd51e29ce8e071960fe8edea442a502d64
Ahmed <jesuisahmedn@gmail.com>
2018-08-12 23:54:10 +0300

    Use android icons instead of fontawesome icons, modularize replacements of fragments
    Since fontawesome requires listing of license for usage of their icons, it's simmpler
    to use the built-in android icons so the fonts were switched out. Also the code for
    adding fragments has been simplified into one method. This allows for cleaner code

--- app/src/main/java/com/schedulearn/schedulearn/MainActivity.java
+++ app/src/main/java/com/schedulearn/schedulearn/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -16,30 +17,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragment_container, new AgendaFragment())
-                .commit();
+        addFragmentFromId(bottomNavigationView.getSelectedItemId());
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment currFragment = null;
-                    switch (item.getItemId()) {
-                        case R.id.action_agenda:
-                            currFragment = new AgendaFragment();
-                            break;
-                        case R.id.action_connections:
-                            currFragment = new ConnectionsFragment();
-                            break;
-                        case R.id.action_profile:
-                            currFragment = new ProfileFragment();
-                            break;
-                    }
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.fragment_container, currFragment)
-                            .commit();
+                    addFragmentFromId(item.getItemId());
                     return true;
                 }
         };
+
+    private void addFragmentFromId(int id) {
+        Fragment currFragment = null;
+        switch (id) {
+            case R.id.action_agenda:
+                currFragment = new AgendaFragment();
+                break;
+            case R.id.action_connections:
+                currFragment = new ConnectionsFragment();
+                break;
+            case R.id.action_profile:
+                currFragment = new ProfileFragment();
+                break;
+        }
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, currFragment)
+                .commit();
+    }
 }
